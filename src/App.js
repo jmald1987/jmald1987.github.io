7990da9e7e65079f24cab6bc3db68d4357357963
jmald1987 <77485004+jmald1987@users.noreply.github.com>
2023-03-06 00:32:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jmald1987"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow me on git
         </a>
       </header>
     </div>
